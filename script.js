c9bf053cb7a1aa7bb4f23d6b1b287b80ca9d3e23
Willen Andrade <71357915+WillenAndrade@users.noreply.github.com>
2023-03-12 12:22:52 -0700

    Adding new validations

--- script.js
+++ script.js
@@ -11,62 +11,73 @@ form.addEventListener("submit", (e) => {
 })
 
 function checkInputs() {
-    //get the values of the inputs
+    
     const usernameValue     = username.value.trim()
     const emailValue        = email.value.trim()
     const passwordValue     =password.value.trim()
     const password2Value    = password2.value.trim()
 
+
+
     if(usernameValue === '') {
-        //show error
-        //add the error class
         setErrorFor(username, 'Username cannot be blank')
     } else {
-        //add success class
         setSuccessFor(username)
     }
 
     if( emailValue === '') {
-        //show error
-        //add the error class
         setErrorFor(email, 'Email cannot be blank')
+    } else if (!validateEmail(emailValue)) {
+        setErrorFor(email, 'This is not a valid email')
     } else {
-        //add success class
         setSuccessFor(email)
     }
 
+
     if( passwordValue === '') {
-        //show error
-        //add the error class
         setErrorFor(password, 'Password cannot be blank')
+    } else if (!validatePassword(passwordValue)) {
+        setErrorFor(password, 'This is not a valid Password')
     } else {
-        //add success class
         setSuccessFor(password)
     }
 
     if( password2Value === '') {
-        //show error
-        //add the error class
         setErrorFor(password2, 'Second password cannot be blank')
     } else if (passwordValue !== password2Value) {
         setErrorFor(password2, 'The password does not match!')
     } else {
-        //add success class
         setSuccessFor(password2)
     }
 
-
-
 }
+
+function validatePassword(senha) {
+    var reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+    if(reg.test(senha)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validateEmail(email) {
+    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+    if (reg.test(email)){
+    return true
+    } else {
+    return false
+           }
+    } 
 
 function setErrorFor(input, message) {
     const formControl = input.parentElement  //.form-control
     const small  = formControl.querySelector('small')
 
-    //add error message inside the small tag
+    
     small.innerText = message
 
-    //add error class
+    
 
     formControl.className = 'form-control error'
 }
